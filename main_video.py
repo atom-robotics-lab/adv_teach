168701d46a45077cbbea083be43aa08bda08c216
Naman Malik <namanmalik0210@gmail.com>
2022-10-15 12:39:22 +0530

    added controller code to main_video.py and added arduino code

--- main_video.py
+++ main_video.py
@@ -1,6 +1,8 @@
 import cv2
 from simple_facerec import SimpleFacerec
 from line import Line_detection
+import serial
+import time
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
@@ -10,6 +12,8 @@ sfr.load_encoding_images("images/")
 # Load Camera
 cap = cv2.VideoCapture(0)
 
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+
 
 while True:
     ret, frame = cap.read()
@@ -18,19 +22,10 @@ while True:
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-        sub = 320 - int((x1 + x2)/2)
-        if sub > 0:
-            print("turn left")
-            print(sub)
-        if sub < 0:
-            print("turn right")
-            print(sub)
-        if -10 < sub < 10:
-            print(sub)
-            print("ggwp")
 
-
-
+        sub = (frame.shape[1]/2) - int((x1 + x2)/2)
+        arduino.write(bytes(sub, 'utf-8'))
+        time.sleep(0.05)
 
         cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
